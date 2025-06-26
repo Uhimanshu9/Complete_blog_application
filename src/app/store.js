73fc91fbe63d528722e184affd7c47c1e095891b
Uhimanshu9 <dev.himanshu.ai@gamil.com>
2025-06-26 22:34:12 +0530

    adding reducer in store for auth

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
+import authReducer from './authSlice' // adjust the path if needed
+
+
 export const store = configureStore({
   reducer: {
-
+     auth: authReducer,
   },
 })
